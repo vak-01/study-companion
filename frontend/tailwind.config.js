dc53fc9cfa14fc3d6437ba077d86543887b8a7b2
vak-01 <78302047+vak-01@users.noreply.github.com>
2023-12-18 15:16:43 +0530

    Configure tailwind css

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+    './node_modules/preline/preline.js',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    require('@tailwindcss/forms'),
+    require('preline/plugin'),
+  ],
+}
+
