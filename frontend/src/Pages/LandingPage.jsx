c79d45c9418dec701aca4db62282621bf2eee0f2
vak-01 <78302047+vak-01@users.noreply.github.com>
2023-12-20 11:34:19 +0530

    fix landing page

--- frontend/src/Pages/LandingPage.jsx
+++ frontend/src/Pages/LandingPage.jsx
@@ -2,13 +2,13 @@
 import React, { useState } from 'react';
 import LoginComponent from '../components/Login';
 
-function HomePage(){
+function LandingPage(){
     const [showModal, setShowModal] = useState(false);
 
     return (
-        <>
+        <div class="p-0 m-0 min-h-screen bg-slate-900 bg-gradient-to-b from-violet-600/[.15] via-transparent w-auto">
             {/* <!-- ========== HEADER ========== --> */}
-            <header class="sticky top-0 flex flex-wrap md:justify-start md:flex-nowrap z-50 w-full text-sm">
+            <header class="sticky top-2 flex flex-wrap md:justify-start md:flex-nowrap z-50 w-full text-sm">
                 <nav class="mt-0 relative max-w-[85rem] w-full bg-white border border-gray-200 rounded-[36px] mx-2 py-3 px-4 md:flex md:items-center md:justify-between md:py-0 md:px-6 lg:px-8 xl:mx-auto dark:bg-gray-800 dark:border-gray-700" aria-label="Global">
                     <div class="flex items-center justify-between">
                         <a class="flex-none text-xl font-semibold dark:text-white" href="#" aria-label="Brand">Study Copilot</a>
@@ -210,7 +210,7 @@ function HomePage(){
                 </div>
                 {/* <!-- End Grid --> */}
             </footer>
-        </>
+        </div>
     )
 }
 
@@ -225,4 +225,4 @@ function Modal({ showModal, setShowModal }) {
     );
 }
 
-export default HomePage;
+export default LandingPage;
