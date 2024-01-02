a1259ce89c10bab71498e7013ba15b1f8fe917d0
vak-01 <78302047+vak-01@users.noreply.github.com>
2024-01-02 21:19:41 +0530

    Add API endpoint for Token authentication

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from rest_framework.authtoken.views import obtain_auth_token
 
 urlpatterns = [
+    path('api-token-auth/', obtain_auth_token, name='api_token_auth'),
     path('admin/', admin.site.urls),
 ]
