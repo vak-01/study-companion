6db9d11aa7595aed25e43aeb8a78c431e83b21ca
vak-01 <78302047+vak-01@users.noreply.github.com>
2024-01-29 20:28:36 +0530

    Add typing effect to landing page

--- frontend/src/Pages/LandingPage.jsx
+++ frontend/src/Pages/LandingPage.jsx
@@ -1,16 +1,79 @@
 // component to display the homepage - contains a navbar at the top and a hero section below the navbar
-import React, { useState } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import LoginComponent from '../components/Login';
 import RegisterComponent from '../components/Register';
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
+import './Landing.css';
 
-function LandingPage(){
+function LandingPage() {
     const [showModal, setShowModal] = useState(false);
+    const [text, setText] = useState('');
+    const [isDeleting, setIsDeleting] = useState(false);
+    const [loopNum, setLoopNum] = useState(0);
+    // const typingSpeedRef = useRef(600);
+    const [typingSpeed, setTypingSpeed] = useState(0);
+    // const isDeleting = useRef(false);
+
+    const phrases = ['Study Copilot', 'AI Assistance', 'Learning goals'];
+
+    useEffect(() => {
+        const typing = setTimeout(() => {
+        
+        if (isDeleting && text === '' && loopNum === phrases.length) {
+              return; 
+          }
+
+          if (isDeleting) {
+            setText((oldText) => oldText.substring(0, oldText.length - 1));
+            setTypingSpeed(100);
+          } else {
+            setText(phrases[loopNum].substring(0, text.length + 1));
+            setTypingSpeed(200);
+          }
+      
+          if (!isDeleting && text === phrases[loopNum]) {
+            setTimeout(() => setIsDeleting(true), 500);
+          } else if (isDeleting && text === '') {
+            setIsDeleting(false);
+            setLoopNum((prevLoopNum) => (prevLoopNum + 1) % phrases.length);
+          }
+        }, typingSpeed);
+      
+        return () => clearTimeout(typing);
+      }, [text, isDeleting, loopNum]);
+
+    useEffect(() => {
+        const observer = new IntersectionObserver(
+          (entries) => {
+            entries.forEach((entry) => {
+              if (entry.isIntersecting) {
+                entry.target.classList.add('flyIn');
+              }
+            });
+          },
+          {
+            root: null,
+            rootMargin: '-10%',
+            threshold: 0.1,
+          }
+        );
+    
+        const flyviewElements = document.querySelectorAll('.flyview');
+        flyviewElements.forEach((element, index) => {
+          element.style.setProperty('--order', index);
+          observer.observe(element);
+        });
+    
+        return () => {
+          flyviewElements.forEach((element) => observer.unobserve(element));
+        };
+      }, []);
 
     return (
-        <div className="p-0 m-0 min-h-screen bg-slate-900 bg-gradient-to-b from-violet-600/[.15] via-transparent w-auto">
+        <div className="p-0 m-0 min-h-screen bg-slate-900 bg-gradient-to-b from-violet-600/[.15] via-transparent w-auto overflow-x-clip">
             {/* <!-- ========== HEADER ========== --> */}
-            <header className="sticky top-2 flex flex-wrap md:justify-start md:flex-nowrap z-50 w-full text-sm">
+            <div className='ai'>
+            <header className="sticky top-2 flex flex-wrap md:justify-start md:flex-nowrap z-50 w-full text-sm min-w-max">
                 <nav className="mt-0 relative max-w-[85rem] w-full bg-white border border-gray-200 rounded-[36px] mx-2 py-3 px-4 md:flex md:items-center md:justify-between md:py-0 md:px-6 lg:px-8 xl:mx-auto dark:bg-gray-800 dark:border-gray-700" aria-label="Global">
                     <div className="flex items-center justify-between">
                         <a className="flex-none text-xl font-semibold dark:text-white" href="#" aria-label="Brand">Study Copilot</a>
@@ -56,10 +119,12 @@ function LandingPage(){
                         {/* <!-- End Announcement Banner --> */}
 
                         {/* <!-- Title --> */}
-                        <div className="max-w-3xl text-center mx-auto">
-                            <h1 className="block font-medium text-gray-200 text-4xl sm:text-5xl md:text-6xl lg:text-7xl">
-                                Supercharge your learning journey with Study Copilot
-                            </h1>
+                        <div className="max-w-3xl text-center mx-auto typing-container">
+                            <span className="block font-medium text-gray-200 text-4xl sm:text-5xl md:text-6xl lg:text-7xl">
+                                Supercharge your learning journey with
+                            </span>
+                            <span className="font-medium text-gray-200 text-4xl sm:text-5xl md:text-6xl lg:text-7xl" id="sentence">{text}</span>
+                            <span className="input-cursor"></span>
                         </div>
                         {/* <!-- End Title --> */}
 
@@ -70,19 +135,20 @@ function LandingPage(){
                         {/* <!-- Buttons --> */}
                         <div className="text-center">
                             <a className="inline-flex justify-center items-center font-bold gap-x-3 text-center bg-gradient-to-tl from-blue-600 to-violet-900 shadow-lg shadow-transparent  border border-transparent text-white text-sm rounded-full py-3 px-6  hover:scale-105 hover:text-white" href="#"
-                             onClick={(e) => {
-                                e.preventDefault();
-                                setShowModal(true);
-                            }}
+                                onClick={(e) => {
+                                    e.preventDefault();
+                                    setShowModal(true);
+                                }}
                             >
                                 Sign up now
                                 <svg className="flex-shrink-0 w-4 h-4" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><path d="m9 18 6-6-6-6" /></svg>
                             </a>
-                            {showModal && <Modal Component = {RegisterComponent} showModal={showModal} setShowModal={setShowModal} />}
+                            {showModal && <Modal Component={RegisterComponent} showModal={showModal} setShowModal={setShowModal} />}
                         </div>
                         {/* <!-- End Buttons --> */}
                     </div>
                 </div>
+            </div>
             </div>
             {/* <!-- End Hero --> */}
 
@@ -99,7 +165,7 @@ function LandingPage(){
 
                             {/* <!-- Tab Navs --> */}
                             <nav className="grid gap-4 mt-5 md:mt-10" aria-label="Tabs" role="tablist">
-                                <button type="button" className="hs-tab-active:bg-white hs-tab-active:shadow-md hs-tab-active:hover:border-transparent text-start hover:bg-gray-200 p-4 md:p-5 rounded-xl dark:hs-tab-active:bg-slate-900 dark:hover:bg-gray-700 dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600 active" id="tabs-with-card-item-1" data-hs-tab="#tabs-with-card-1" aria-controls="tabs-with-card-1" role="tab">
+                                <button type="button" className="hs-tab-active:bg-white hs-tab-active:shadow-md hs-tab-active:hover:border-transparent text-start hover:bg-gray-200 p-4 md:p-5 rounded-xl dark:hs-tab-active:bg-slate-900 dark:hover:bg-gray-700 dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600 active flyview" id="tabs-with-card-item-1" data-hs-tab="#tabs-with-card-1" aria-controls="tabs-with-card-1" role="tab">
                                     <span className="flex">
                                         <svg className="flex-shrink-0 mt-1 w-6 h-6 text-blue-600" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><rect width="18" height="10" x="3" y="11" rx="2" /><circle cx="12" cy="5" r="2" /><path d="M12 7v4" /><line x1="8" x2="8" y1="16" y2="16" /><line x1="16" x2="16" y1="16" y2="16" /></svg>
                                         <span className="grow ms-6">
@@ -109,7 +175,7 @@ function LandingPage(){
                                     </span>
                                 </button>
 
-                                <button type="button" className="hs-tab-active:bg-white hs-tab-active:shadow-md hs-tab-active:hover:border-transparent text-start hover:bg-gray-200 p-4 md:p-5 rounded-xl dark:hs-tab-active:bg-slate-900 dark:hover:bg-gray-700 dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600" id="tabs-with-card-item-2" data-hs-tab="#tabs-with-card-2" aria-controls="tabs-with-card-2" role="tab">
+                                <button type="button" className="hs-tab-active:bg-white hs-tab-active:shadow-md hs-tab-active:hover:border-transparent text-start hover:bg-gray-200 p-4 md:p-5 rounded-xl dark:hs-tab-active:bg-slate-900 dark:hover:bg-gray-700 dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600 flyview" id="tabs-with-card-item-2" data-hs-tab="#tabs-with-card-2" aria-controls="tabs-with-card-2" role="tab">
                                     <span className="flex">
                                         <svg className="flex-shrink-0 mt-1 w-6 h-6 text-white" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><path d="M7 10v12" /><path d="M15 5.88 14 10h5.83a2 2 0 0 1 1.92 2.56l-2.33 8A2 2 0 0 1 17.5 22H4a2 2 0 0 1-2-2v-8a2 2 0 0 1 2-2h2.76a2 2 0 0 0 1.79-1.11L12 2h0a3.13 3.13 0 0 1 3 3.88Z" /></svg>
                                         <span className="grow ms-6">
@@ -119,7 +185,7 @@ function LandingPage(){
                                     </span>
                                 </button>
 
-                                <button type="button" className="hs-tab-active:bg-white hs-tab-active:shadow-md hs-tab-active:hover:border-transparent text-start hover:bg-gray-200 p-4 md:p-5 rounded-xl dark:hs-tab-active:bg-slate-900 dark:hover:bg-gray-700 dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600" id="tabs-with-card-item-3" data-hs-tab="#tabs-with-card-3" aria-controls="tabs-with-card-3" role="tab">
+                                <button type="button" className="hs-tab-active:bg-white hs-tab-active:shadow-md hs-tab-active:hover:border-transparent text-start hover:bg-gray-200 p-4 md:p-5 rounded-xl dark:hs-tab-active:bg-slate-900 dark:hover:bg-gray-700 dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600 flyview" id="tabs-with-card-item-3" data-hs-tab="#tabs-with-card-3" aria-controls="tabs-with-card-3" role="tab">
                                     <span className="flex">
                                         <svg className="flex-shrink-0 mt-1 w-6 h-6 text-white" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><path d="M2 3h6a4 4 0 0 1 4 4v14a3 3 0 0 0-3-3H2z" /><path d="M22 3h-6a4 4 0 0 0-4 4v14a3 3 0 0 1 3-3h7z" /></svg>              <span className="grow ms-6">
                                             <span className="block text-lg font-semibold hs-tab-active:text-blue-600 text-gray-800 dark:hs-tab-active:text-blue-500 dark:text-gray-200">Flashcards powered by Markdown</span>
@@ -128,7 +194,7 @@ function LandingPage(){
                                     </span>
                                 </button>
 
-                                <button type="button" className="hs-tab-active:bg-white hs-tab-active:shadow-md hs-tab-active:hover:border-transparent text-start hover:bg-gray-200 p-4 md:p-5 rounded-xl dark:hs-tab-active:bg-slate-900 dark:hover:bg-gray-700 dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600" id="tabs-with-card-item-3" data-hs-tab="#tabs-with-card-3" aria-controls="tabs-with-card-3" role="tab">
+                                <button type="button" className="hs-tab-active:bg-white hs-tab-active:shadow-md hs-tab-active:hover:border-transparent text-start hover:bg-gray-200 p-4 md:p-5 rounded-xl dark:hs-tab-active:bg-slate-900 dark:hover:bg-gray-700 dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600 flyview" id="tabs-with-card-item-3" data-hs-tab="#tabs-with-card-3" aria-controls="tabs-with-card-3" role="tab">
                                     <span className="flex">
                                         <svg className="flex-shrink-0 mt-1 w-6 h-6 text-white" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><path d="M16 21v-2a4 4 0 0 0-4-4H6a4 4 0 0 0-4 4v2" /><circle cx="9" cy="7" r="4" /><path d="M22 21v-2a4 4 0 0 0-3-3.87" /><path d="M16 3.13a4 4 0 0 1 0 7.75" /></svg>
                                         <span className="grow ms-6">
@@ -192,7 +258,7 @@ function LandingPage(){
 
                     {/* <!-- Social Brands --> */}
                     <div className="mt-3 space-x-2">
-                        
+
                         <a className="inline-flex justify-center items-center w-10 h-10 text-center text-gray-500 hover:bg-gray-100 rounded-full focus:outline-none focus:ring-2 focus:ring-blue-600 focus:ring-offset-2 focus:ring-offset-white transition dark:text-gray-500 dark:hover:text-gray-200 dark:hover:bg-gray-800" href="#">
                             <svg className="flex-shrink-0 w-3.5 h-3.5" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
                                 <path d="M5.026 15c6.038 0 9.341-5.003 9.341-9.334 0-.14 0-.282-.006-.422A6.685 6.685 0 0 0 16 3.542a6.658 6.658 0 0 1-1.889.518 3.301 3.301 0 0 0 1.447-1.817 6.533 6.533 0 0 1-2.087.793A3.286 3.286 0 0 0 7.875 6.03a9.325 9.325 0 0 1-6.767-3.429 3.289 3.289 0 0 0 1.018 4.382A3.323 3.323 0 0 1 .64 6.575v.045a3.288 3.288 0 0 0 2.632 3.218 3.203 3.203 0 0 1-.865.115 3.23 3.23 0 0 1-.614-.057 3.283 3.283 0 0 0 3.067 2.277A6.588 6.588 0 0 1 .78 13.58a6.32 6.32 0 0 1-.78-.045A9.344 9.344 0 0 0 5.026 15z" />
@@ -203,7 +269,7 @@ function LandingPage(){
                                 <path d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.012 8.012 0 0 0 16 8c0-4.42-3.58-8-8-8z" />
                             </svg>
                         </a>
-                        
+
                     </div>
                     {/* <!-- End Social Brands --> */}
                 </div>
@@ -215,12 +281,12 @@ function LandingPage(){
 
 function Modal({ Component, showModal, setShowModal }) {
     return (
-      <div className="fixed inset-0 z-50 flex items-center justify-center">
-        <div className="absolute inset-0 bg-black opacity-80" onClick={() => setShowModal(false)}></div>
-        <div className="absolute p-6 rounded shadow-lg">
-          <Component />
+        <div className="fixed inset-0 z-50 flex items-center justify-center">
+            <div className="absolute inset-0 bg-black opacity-80" onClick={() => setShowModal(false)}></div>
+            <div className="absolute p-6 rounded shadow-lg">
+                <Component />
+            </div>
         </div>
-      </div>
     );
 }
 
