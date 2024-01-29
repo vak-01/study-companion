772a193b875e56079980a672551a9b3886460949
vak-01 <78302047+vak-01@users.noreply.github.com>
2024-01-29 20:14:01 +0530

    Add registration functionality and goals endpoint

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -15,12 +15,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from rest_framework.authtoken.views import obtain_auth_token
 from . import views
+import goals
 
 urlpatterns = [
     path('api-token-auth/', obtain_auth_token, name='api_token_auth'),
+    path('register/', views.register, name='register'),
     path('logout/', views.logout, name='logout'),
+    path('goals/', include('goals.urls')),
     path('admin/', admin.site.urls)
 ]
--- backend/backend/views.py
+++ backend/backend/views.py
@@ -2,6 +2,7 @@ from rest_framework import status
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
+from .serializers import UserSerializer
 
 @api_view(['GET'])
 @permission_classes([IsAuthenticated])
@@ -9,3 +10,12 @@ def logout(request):
     # simply delete the token to force a login
     request.user.auth_token.delete()
     return Response(status=status.HTTP_200_OK)
+
+@api_view(['POST'])
+def register(request):
+    serializer = UserSerializer(data=request.data)
+    if serializer.is_valid():
+        user = serializer.save()
+        if user:
+            return Response(status=status.HTTP_201_CREATED)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
