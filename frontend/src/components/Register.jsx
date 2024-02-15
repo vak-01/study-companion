92ea2dee63af68f075caa309abf7aa636354aee0
vak-01 <78302047+vak-01@users.noreply.github.com>
2024-02-15 22:40:09 +0530

    change #2

--- frontend/src/components/Register.jsx
+++ frontend/src/components/Register.jsx
@@ -62,7 +62,9 @@ function RegisterComponent() {
               <div className="py-3 flex items-center text-xs text-gray-400 uppercase before:flex-[1_1_0%] before:border-t before:border-gray-200 before:me-6 after:flex-[1_1_0%] after:border-t after:border-gray-200 after:ms-6 dark:text-gray-500 dark:before:border-gray-600 dark:after:border-gray-600">Or</div>
 
               {/* <!-- Form --> */}
-              <form onSubmit={handleSubmit}>
+              <form 
+              // onSubmit={handleSubmit}
+              >
                 <div className="grid gap-y-4">
                   {/* <!-- Form Group --> */}
                   <div>
